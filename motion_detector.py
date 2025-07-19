2f58f07dc29cfa14c68633cca9fafce9ef11d41b
AgusMuzi79 <agusmuzi79@gmail.com>
2025-07-19 16:27:55 -0300

    Project finished with a gif to show how it works

--- motion_detector.py
+++ motion_detector.py
@@ -1,10 +1,16 @@
 import cv2
+from datetime import datetime
+import pandas as pd
 
 first_frame = None # Variable to store the first frame for motion detection
 video = cv2.VideoCapture(0) # Initialize video capture from the default camera
+status_list = [None, None] # List to keep track of motion status
+times = [] # List to keep track of timestamps of motion events
+df = pd.DataFrame(columns=["Start", "End"]) # DataFrame to store start and end times of motion events
 
 while True:
     check, frame = video.read() # Read a frame from the video capture
+    status = 0 # Initialize status variable
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert the frame to grayscale
     gray = cv2.GaussianBlur(gray, (21, 21), 0) # Apply Gaussian blur to the grayscale frame for better motion detection
@@ -20,10 +26,19 @@ while True:
     (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Find contours in the thresholded frame
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 500: # Ignore small contours
+        if cv2.contourArea(contour) < 250: # Ignore small contours
             continue
+        status = 1 # Set status to 1 if a significant contour is found
         (x, y, w, h) = cv2.boundingRect(contour) # Get the bounding box for the contour
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) # Draw a rectangle around the detected motion
+    status_list.append(status) # Append the current status to the status list
+
+
+    if status_list[-1] == 1 and status_list[-2] == 0: # Check for motion start
+        times.append(datetime.now()) # Record the time when motion starts
+    if status_list[-1] == 0 and status_list[-2] == 1: # Check for motion end
+        times.append(datetime.now()) # Record the time when motion ends
+
 
     cv2.imshow("Gray frame", gray) # Display the frame
     cv2.imshow("Delta Frame", delta_frame) # Display the delta frame
@@ -32,7 +47,17 @@ while True:
 
     key = cv2.waitKey(1) # Wait for a key press
     if key == ord('q'): # If 'q' is pressed, exit the loop
+        if status == 1: # If motion was active when quitting
+            times.append(datetime.now()) # Record the time when motion ends if it was active
         break
 
+
+print(status_list) # Print the status list to the console
+print(times) # Print the times of motion events to the console
+
+for i in range(0, len(times), 2): # Iterate through the times list in pairs
+    df.loc[len(df)] = {"Start": times[i], "End": times[i + 1]} # Append start and end times to the DataFrame
+
+df.to_csv("Times.csv") # Save the DataFrame to a CSV file
 video.release() # Release the video capture object
 cv2.destroyAllWindows() # Close all OpenCV windows
